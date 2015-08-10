8ce5b113b25f757d443418b28d3bc008e90f22d5
Ramiro Acoglanis <acoglanis.ram@gmail.com>
2015-08-10 17:46:24 -0300

    creacion de la clase Positions / PositionsDAO

---
+++ src/java/com/corp/globant/MODEL/Position.java
@@ -0,0 +1,36 @@
+package com.corp.globant.MODEL;
+/**
+ *
+ * @author ramiro.acoglanis
+ */
+public class Position {
+    private String id;
+    private String desc;
+    private String ad;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+    
+    
+}
